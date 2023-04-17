5552ab79f70422ed8f05b41da006b079c41d58e9
yarin <shm.yarin@gmail.com>
2023-04-17 09:42:32 +0300

    Fix

--- Api.fruit.test/test_Api.py
+++ Api.fruit.test/test_Api.py
@@ -6,42 +6,38 @@ from mainApi import*
 class TestAPI:
 
 
+    def test_1_banana(self):
+        assert Check_status_code(code_site('https://fruityvice.com/api/fruit/banana')) == True
+
+    def test_2_mazda(self):
+        assert Check_status_code(code_site('https://fruityvice.com/api/fruit/Mazda')) == False
+
+    def test_3_Kiwi(self):
+        assert Does_it_contain_number('https://fruityvice.com/api/fruit/kiwi', 66, "id") == True
+
+    def test_4_kiwi(self):
+        assert Does_it_contain_number('https://fruityvice.com/api/fruit/kiwi', 5, "id") == True
+
+    def test_5_Kiwi(self):
+        assert Does_it_contain_value('https://fruityvice.com/api/fruit/kiwi','Solanaceae', 'family')
+
     def test_6_all_fruits(self):
         assert Check_how_many_Jsons('https://fruityvice.com/api/fruit/all') > 7
+
+    def test_7_mango(self):
+        assert Check_status_code(code_site('https://fruityvice.com/api/fruit/mango')) == True
+
+    def test_8_strawberry_id(self):
+        assert Does_it_contain_number('https://fruityvice.com/api/fruit/strawberry', 3, "id") == True
+
+    def test_9_strawberry_name(self):
+        assert Does_it_contain_value('https://fruityvice.com/api/fruit/strawberry', 'Strawberry', 'name') == True
 
     def test_10_tomato_fat(self):
         assert How_many_nut_values('https://fruityvice.com/api/fruit/tomato','fat') == 0.2
 
-#class TestOther:
+    def test_11_kiwi_fat(self):
+        assert How_many_nut_values('https://fruityvice.com/api/fruit/kiwi', 'fat') == 0.5
 
-    #
-    # def test_1_banana(self):
-    #     assert Check_status_code(code_site('https://fruityvice.com/api/fruit/banana')) == True
-    #
-    # def test_2_mazda(self):
-    #     assert Check_status_code(code_site('https://fruityvice.com/api/fruit/Mazda')) == False
-    #
-    # def test_3_Kiwi(self):
-    #     assert Does_it_contain_number('https://fruityvice.com/api/fruit/kiwi', 66, "id") == True
-    #
-    # def test_4_kiwi(self):
-    #     assert Does_it_contain_number('https://fruityvice.com/api/fruit/kiwi', 5, "id") == True
-    #
-    # def test_5_Kiwi(self):
-    #     assert Does_it_contain_value('https://fruityvice.com/api/fruit/kiwi','Solanaceae', 'family')
-    #
-    # def test_7_mango(self):
-    #     assert Check_status_code(code_site('https://fruityvice.com/api/fruit/mango')) == True
-    #
-    # def test_8_strawberry_id(self):
-    #     assert Does_it_contain_number('https://fruityvice.com/api/fruit/strawberry', 3, "id") == True
-    #
-    # def test_9_strawberry_name(self):
-    #     assert Does_it_contain_value('https://fruityvice.com/api/fruit/strawberry', 'Strawberry', 'name') == True
-    #
-    #
-    # def test_11_kiwi_fat(self):
-    #     assert How_many_nut_values('https://fruityvice.com/api/fruit/kiwi', 'fat') == 0.5
-    #
-    # def test_12_mango_calories(self):
-    #     assert How_many_nut_values('https://fruityvice.com/api/fruit/mango', 'calories') == 60
+    def test_12_mango_calories(self):
+        assert How_many_nut_values('https://fruityvice.com/api/fruit/mango', 'calories') == 60
--- Selenium.bank.test/main.py
+++ Selenium.bank.test/main.py
@@ -8,49 +8,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome()
 url = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/'
 
 
-def OpenWeb():
-    return driver.get(url), driver.maximize_window(), time.sleep(2)
+def OpenWeb(driver):
+    driver.get(url)
 
 
-def find_element(Css):
+def find_element(driver, Css):
     element = driver.find_element(By.CSS_SELECTOR, Css)
     return element
 
-def Login_Customer(CssFromList):
-    CustomerLogin = find_element('body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(1) > button').click()
+def Login_Customer(driver, CssFromList):
+    time.sleep(2)
+    CustomerLogin = find_element(driver ,'body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(1) > button').click()
     time.sleep(1)
-    UserName = find_element(CssFromList).click()
+    UserName = find_element(driver,CssFromList).click()
     time.sleep(1)
-    LoginButton = find_element('body > div > div > div.ng-scope > div > form > button').click()
-    time.sleep(1)
-
-def Diposit_by_Customer(Amount):
-    DepositList = find_element('body > div > div > div.ng-scope > div > div:nth-child(5) > button:nth-child(2)').click()
-    time.sleep(1)
-    DepositAmount = find_element('body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > div > input').send_keys(Amount)
-    time.sleep(1)
-    DepositFinalButton = find_element('body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > button').click()
-
-
-def Withdraw_by_Customer(Amount):
-    WithdrawlList = find_element('body > div > div > div.ng-scope > div > div:nth-child(5) > button:nth-child(3)').click()
-    time.sleep(1)
-    WithdrawAmount = find_element('body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > div > input').send_keys(Amount)
-    time.sleep(1)
-    WithdrawFinalButton = find_element('body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > button').click()
+    LoginButton = find_element(driver,'body > div > div > div.ng-scope > div > form > button').click()
     time.sleep(1)
 
+def Diposit_by_Customer(driver, Amount):
+    DepositList = find_element(driver,'body > div > div > div.ng-scope > div > div:nth-child(5) > button:nth-child(2)').click()
+    time.sleep(1)
+    DepositAmount = find_element(driver,'body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > div > input').send_keys(Amount)
+    time.sleep(1)
+    DepositFinalButton = find_element(driver,'body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > button').click()
+
+
+def Withdraw_by_Customer(driver, Amount):
+    WithdrawlList = find_element(driver, 'body > div > div > div.ng-scope > div > div:nth-child(5) > button:nth-child(3)').click()
+    time.sleep(1)
+    WithdrawAmount = find_element(driver, 'body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > div > input').send_keys(Amount)
+    time.sleep(1)
+    WithdrawFinalButton = find_element(driver, 'body > div > div > div.ng-scope > div > div.container-fluid.mainBox.ng-scope > div > form > button').click()
+    time.sleep(1)
 
 
 
-def Customer_exist_in_listCust(first_name):
+
+def Customer_exist_in_listCust(driver, first_name):
     x = False
-    tabale = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > table')
-    body = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > table > tbody')
+    tabale = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > table')
+    body = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > table > tbody')
     rows = driver.find_elements(By.TAG_NAME, "tr")
     for i in rows:
         columns = i.find_elements(By.TAG_NAME, "td")
@@ -60,24 +60,24 @@ def Customer_exist_in_listCust(first_name):
     return x
 
 
-def Add_New_customer(firstName, lastName, postalCode):
-    ManagerLogin = find_element('body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button').click()
+def Add_New_customer(driver, firstName, lastName, postalCode):
+    ManagerLogin = find_element(driver, 'body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button').click()
     time.sleep(1)
-    AddCustomerList = find_element('body > div > div > div.ng-scope > div > div.center > button:nth-child(1)').click()
+    AddCustomerList = find_element(driver, 'body > div > div > div.ng-scope > div > div.center > button:nth-child(1)').click()
     time.sleep(1)
-    NewF_name = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(1) > input').send_keys(firstName)
-    NewL_name = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(2) > input').send_keys(lastName)
+    NewF_name = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(1) > input').send_keys(firstName)
+    NewL_name = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(2) > input').send_keys(lastName)
     time.sleep(1)
-    NewPcode = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(3) > input').send_keys(postalCode)
+    NewPcode = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > div:nth-child(3) > input').send_keys(postalCode)
     time.sleep(1)
-    AddCustomerButton = find_element('body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > button').click()
+    AddCustomerButton = find_element(driver, 'body > div > div > div.ng-scope > div > div.ng-scope > div > div > form > button').click()
     time.sleep(3)
 
-def Delete_customer_by_maneger(Css):
-    ManagerLogin = find_element('body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button').click()
+def Delete_customer_by_maneger(driver, Css):
+    ManagerLogin = find_element(driver, 'body > div > div > div.ng-scope > div > div.borderM.box.padT20 > div:nth-child(3) > button').click()
     time.sleep(1)
-    CustomersList = find_element('body > div > div > div.ng-scope > div > div.center > button:nth-child(3)').click()
+    CustomersList = find_element(driver, 'body > div > div > div.ng-scope > div > div.center > button:nth-child(3)').click()
     time.sleep(1)
-    DelcusButton = find_element(Css).click()
+    DelcusButton = find_element(driver, Css).click()
     time.sleep(2)
 
